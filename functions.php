c950bb59710dab6791bd796739fa5f19673b1722
Greg Annandale <g.annandale@gmail.com>
2016-01-21 16:25:08 +0000

    Added CC style guide JS

--- functions.php
+++ functions.php
@@ -119,16 +119,13 @@ function ccw_countries_scripts() {
 
 	wp_enqueue_style( 'ccw-countries-style', get_stylesheet_uri() );
 
+	wp_enqueue_script('ccw-countries-style-guide-script', get_template_directory_uri() . '/bower_components/code-club/dist/javascripts/code-club.min.js', ['jquery'], null, true);
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'ccw_countries_scripts' );
-
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
 
 /**
  * Custom template tags for this theme.
