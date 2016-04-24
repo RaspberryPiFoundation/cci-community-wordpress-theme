c8d2cc39e7d6a1c4e2eb559c428f4e0f81a9247e
Greg Annandale <g.annandale@gmail.com>
2016-04-24 17:32:41 +0100

    Bower update

--- bower_components/code-club/dist/javascripts/code-club.js
+++ bower_components/code-club/dist/javascripts/code-club.js
@@ -2,10 +2,9 @@
  * code-club.js
  * 
  * Code Club Style Guide - A collection of UI components for styling Code Club websites
- * @version v0.0.1-16
+ * @version v1.0.10
  * @link https://styleguide.codeclubworld.org/
  */
 //  Import Modules from component JavaScript files
 //  import * as another from 'components/foo';
 "use strict";
-//# sourceMappingURL=code-club.js.map
