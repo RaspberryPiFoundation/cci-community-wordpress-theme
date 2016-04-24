088cf210b9509f3571c186949a795385ae88b35b
Greg Annandale <g.annandale@gmail.com>
2016-04-24 23:20:36 +0100

    Header & hero markup fixes

--- header.php
+++ header.php
@@ -22,26 +22,39 @@
 
 <body <?php body_class(); ?>>
 
-<header class="o-banner" role="banner">
-  <div class="o-banner__inner">
-    <figure class="c-logo">
-      <a href="<?= esc_url(home_url('/')); ?>" class="c-logo__link">
-        <img alt="" class="c-logo__image" src="<?php echo get_template_directory_uri(); ?>/bower_components/code-club/dist/images/code-club-logo.svg">
-        <figcaption class="c-logo__appendix"><?php bloginfo('name'); ?></figcaption>
-      </a>
-    </figure>
+<header class="o-nav">
+    <div class="o-nav__container">
+        <div class="c-logo">
+            <a href="<?= esc_url(home_url('/')); ?>" class="c-logo__link">
+                <img alt="Code Club Logo" class="c-logo__image" src="<?php echo get_template_directory_uri(); ?>/bower_components/code-club/dist/images/code-club-logo.svg">
+                <div class="c-logo__appendix"><?php bloginfo('name'); ?></div>
+            </a>
+        </div>
 
-    <nav class="o-nav" role="navigation">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu([
-          'theme_location' => 'primary_navigation',
-          'menu_class' => 'o-nav__list'
-        ]);
-      endif;
-      ?>
-    </nav>
-  </div>
+        <input class="o-nav__toggle-input" id="off-canvas-nav-toggle-input" type="checkbox">
+        <label class="o-nav__toggle-label" for="off-canvas-nav-toggle-input"><span></span></label>
+
+        <nav class="o-nav__list-container">
+        <?php
+        if (has_nav_menu('primary_navigation')) :
+            wp_nav_menu([
+                'theme_location' => 'primary_navigation',
+                'menu_class' => 'o-nav__list',
+                'container' => false
+            ]);
+        endif;
+        ?>
+        </nav>
+    </div>
 </header>
 
-<div class="wrap container" role="document">
+<main class="main">
+
+    <div class="o-hero">
+        <div class="o-hero__body">
+            <h1 class="o-hero__title"><?php #the_field('page_title'); ?></h1>
+            <?php #if ( get_field('page_intro') ): ?>
+            <p class="o-hero__subtitle c-lede"><?php #the_field('page_intro'); ?></p>
+            <?php #endif; ?>
+        </div>
+    </div>
