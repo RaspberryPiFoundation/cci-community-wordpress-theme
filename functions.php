33755245ceff83ea35c70adbccda6d9b50c9af3d
Greg Annandale <g.annandale@gmail.com>
2016-04-24 17:43:16 +0100

    Updated nav class overrides

--- functions.php
+++ functions.php
@@ -155,13 +155,28 @@ require get_template_directory() . '/inc/jetpack.php';
  */
 add_filter('nav_menu_css_class', 'primary_nav_li', 1, 3);
 function primary_nav_li($classes, $item, $args) {
-	$classes[] = 'o-nav__item';
+	switch($args->theme_location) {
+		case 'primary_navigation':
+			$classes[] = 'o-nav__item';
+			break;
+		case (preg_match('/footer_navigation_.*/', $args->theme_location) ? true : false):
+			$classes[] = 'o-footer__list-item';
+			break;
+	}
 	return $classes;
 }
 
-add_filter('wp_nav_menu', 'primary_nav_anchors');
-function primary_nav_anchors($anchorclass) {
-	return preg_replace('/<a /', '<a class="o-nav__link" ', $anchorclass);
+add_filter('nav_menu_link_attributes', 'add_menu_link_classes', 10, 3);
+function add_menu_link_classes($atts, $item, $args) {
+	switch($args->theme_location) {
+		case 'primary_navigation':
+			$atts['class'] = 'o-nav__link';
+			break;
+		case (preg_match('/footer_navigation_.*/', $args->theme_location) ? true : false):
+			$atts['class'] = 'o-footer__list-link';
+			break;
+	}
+	return $atts;
 }
 
 add_filter('nav_menu_css_class', 'current_nav_class', 10, 2);
