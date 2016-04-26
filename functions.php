f1ca4d93783ff40c98ed57a526a6ed275c7c80f3
Greg Annandale <g.annandale@gmail.com>
2016-04-26 12:53:59 +0100

    Removed unneeded feed links from head

--- functions.php
+++ functions.php
@@ -24,9 +24,6 @@ function ccw_countries_setup() {
 	 */
 	load_theme_textdomain( 'ccw_countries', get_template_directory() . '/languages' );
 
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
-
 	/*
 	 * Let WordPress manage the document title.
 	 * By adding theme support, we declare that this theme does not use a
