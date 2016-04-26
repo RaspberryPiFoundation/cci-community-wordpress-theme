d5024bf44752027db0042b01906d182674c5b608
Greg Annandale <g.annandale@gmail.com>
2016-04-26 13:33:25 +0100

    Syntax tweaks

--- footer.php
+++ footer.php
@@ -15,53 +15,53 @@
 
 <footer class="o-footer">
     <div class="c-grid c-grid--v-top">
-        <?php if (has_nav_menu('footer_navigation_1')) : ?>
+        <?php if ( has_nav_menu( 'footer_navigation_1' ) ) : ?>
         <div class="c-col--3">
             <p class="o-footer__heading"><?php nav_menu_name_by_location('footer_navigation_1'); ?></p>
             <?php
             wp_nav_menu([
                 'theme_location' => 'footer_navigation_1',
-                'menu_class' => 'o-footer__list',
-                'container' => false
+                'menu_class'     => 'o-footer__list',
+                'container'      => false
             ]);
             ?>
         </div>
         <?php endif; ?>
 
-        <?php if (has_nav_menu('footer_navigation_2')) : ?>
+        <?php if ( has_nav_menu( 'footer_navigation_2' ) ) : ?>
         <div class="c-col--3">
             <p class="o-footer__heading"><?php nav_menu_name_by_location('footer_navigation_2'); ?></p>
             <?php
             wp_nav_menu([
                 'theme_location' => 'footer_navigation_2',
-                'menu_class' => 'o-footer__list',
-                'container' => false
+                'menu_class'     => 'o-footer__list',
+                'container'      => false
             ]);
             ?>
         </div>
         <?php endif; ?>
 
-        <?php if (has_nav_menu('footer_navigation_3')) : ?>
+        <?php if ( has_nav_menu( 'footer_navigation_3' ) ) : ?>
         <div class="c-col--3">
             <p class="o-footer__heading"><?php nav_menu_name_by_location('footer_navigation_3'); ?></p>
             <?php
             wp_nav_menu([
                 'theme_location' => 'footer_navigation_3',
-                'menu_class' => 'o-footer__list',
-                'container' => false
+                'menu_class'     => 'o-footer__list',
+                'container'      => false
             ]);
             ?>
         </div>
         <?php endif; ?>
 
-        <?php if (has_nav_menu('footer_navigation_4')) : ?>
+        <?php if ( has_nav_menu( 'footer_navigation_4' ) ) : ?>
         <div class="c-col--3">
             <p class="o-footer__heading"><?php nav_menu_name_by_location('footer_navigation_4'); ?></p>
             <?php
             wp_nav_menu([
                 'theme_location' => 'footer_navigation_4',
-                'menu_class' => 'o-footer__list',
-                'container' => false
+                'menu_class'     => 'o-footer__list',
+                'container'      => false
             ]);
             ?>
         </div>
@@ -70,7 +70,7 @@
 
     <div class="c-grid">
         <div class="c-col--9">
-            &copy; <?php echo date('Y') ?>
+            &copy; <?php echo date('Y'); ?>
         </div>
         <div class="c-col--2">
             <ul class="o-footer__social-list">
