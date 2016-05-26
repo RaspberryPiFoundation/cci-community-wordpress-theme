04ebdd95a1f23694138cfb2065c15b75a5d3b749
Greg Annandale <g.annandale@gmail.com>
2016-05-26 14:12:41 +0100

    pot updates

--- footer.php
+++ footer.php
@@ -2,8 +2,6 @@
 /**
  * The template for displaying the footer.
  *
- * Contains the closing of the #content div and all content after.
- *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
  * @package CCW_Countries
