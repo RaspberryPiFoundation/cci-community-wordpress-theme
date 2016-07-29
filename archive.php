8e5f27f888204abbf917841532cd3f9a6e92c85d
Greg Annandale <g.annandale@gmail.com>
2016-07-29 15:19:02 +0100

    Sensible defaults for archive & single pages

--- archive.php
+++ archive.php
@@ -9,43 +9,36 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
 
-		<?php
-		if ( have_posts() ) : ?>
+	<?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
+		<?php get_template_part( 'template-parts/page-header' ); ?>
+
+		<div class="c-page-block c-page-block--alt-block">
+			<div class="c-grid c-grid--h-center">
+				<div class="c-col c-col--8">
 				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+				/* Start the Loop */
+				while ( have_posts() ) : the_post();
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+					/*
+					 * Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					get_template_part( 'template-parts/content', get_post_format() );
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+				endwhile;
 
-			endwhile;
+				the_posts_navigation(); ?>
 
-			the_posts_navigation();
+				</div>
+			</div>
+		</div>
 
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	<?php else:
+		get_template_part( 'template-parts/content', 'none' );
+	endif; ?>
 
 <?php
-get_sidebar();
 get_footer();
--- single.php
+++ single.php
@@ -9,27 +9,28 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<?php get_template_part( 'template-parts/page-header' ); ?>
 
-		<?php
-		while ( have_posts() ) : the_post();
+	<div class="c-page-block">
+		<div class="c-grid c-grid--h-center">
+			<div class="c-col--8">
+			<?php
+			while ( have_posts() ) : the_post();
 
-			get_template_part( 'template-parts/content', get_post_format() );
+				get_template_part( 'template-parts/content', get_post_format() );
 
-			the_post_navigation();
+				the_post_navigation();
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
 
-		endwhile; // End of the loop.
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+			endwhile; // End of the loop.
+			?>
+			</div>
+		</div>
+	</div>
 
 <?php
-get_sidebar();
 get_footer();
