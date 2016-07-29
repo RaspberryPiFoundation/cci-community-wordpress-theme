26832e1b21d97e87ea8d6eb6e30dae2abe24fe70
Greg Annandale <g.annandale@gmail.com>
2016-07-29 14:16:13 +0100

    Formatting

--- template-parts/form-register-club.php
+++ template-parts/form-register-club.php
@@ -7,67 +7,60 @@
 
 if ( empty( $_POST['honeypot'] ) && !empty( $_POST['terms-checkbox'] ) ) {
 
-  // set the club name from the venue name
-  $_POST['club']['name'] = $_POST['club']['venue_attributes']['name'];
-  $json = json_encode( $_POST );
+    // set the club name from the venue name
+    $_POST['club']['name'] = $_POST['club']['venue_attributes']['name'];
+    $json = json_encode( $_POST );
 
-  // POST the form to the CCW API
-  $response = wp_remote_post( CCW_API_URL . '/clubs?welcome_email=' . CCW_API_WELCOME_EMAIL, array(
-    'timeout' => 30,
-    'headers' => array(
-      'Content-Type' => 'application/json',
-      'Content-Type' => 'application/json',
-      'Authorization' => 'Bearer ' . CCW_API_READWRITE_TOKEN,
-      'Accept' => 'application/vnd.codeclubworld.v' . CCW_API_VERSION,
-    ),
-    'body' => $json,
-  ));
+    // POST the form to the CCW API
+    $response = wp_remote_post( CCW_API_URL . '/clubs?welcome_email=' . CCW_API_WELCOME_EMAIL, array(
+        'timeout' => 30,
+        'headers' => array(
+            'Content-Type' => 'application/json',
+            'Content-Type' => 'application/json',
+            'Authorization' => 'Bearer ' . CCW_API_READWRITE_TOKEN,
+            'Accept' => 'application/vnd.codeclubworld.v' . CCW_API_VERSION,
+        ),
+        'body' => $json,
+    ));
 
-  if ( !is_wp_error( $response ) ) {
-    // successful POST, forward to the 'registration success' page
-    if ( 201 == wp_remote_retrieve_response_code( $response ) ) {
-      $_POST = array();
-      wp_safe_redirect( CCW_API_REGISTRATION_SUCCESS_PAGE );
-      exit;
-    } else {
-      // the API returned an error / errors, display them
-      ?>
-      <div class="c-alert c-alert--error">
-        <span class="c-icon c-icon--small c-icon--error c-icon--white"></span>
-        <ul>
+    if ( !is_wp_error( $response ) ) { // POST response
+        if ( 201 == wp_remote_retrieve_response_code( $response ) ) { // successful POST, forward to the 'registration success' page
+            $_POST = array();
+            wp_safe_redirect( CCW_API_REGISTRATION_SUCCESS_PAGE );
+            exit;
+        } else { // the API returned an error / errors, display them ?>
+            <div class="c-alert c-alert--error">
+                <span class="c-icon c-icon--small c-icon--error c-icon--white"></span>
+                <ul>
+                    <?php
+                    $body = json_decode( wp_remote_retrieve_body( $response ) );
+                    echo $body->error.":";
+                    foreach( $body->errors as $key=>$value ) {
+                        echo "<li>".$key." ".$value."</li>";
+                    }
+                    ?>
+                </ul>
+            </div>
         <?php
-          $body = json_decode( wp_remote_retrieve_body( $response ) );
-          echo $body->error.":";
-          foreach( $body->errors as $key=>$value ) {
-            echo "<li>".$key." ".$value."</li>";
-          }
-        ?>
-        </ul>
-      </div>
+        }
+    } else { // the wp_remote_post() function returned an error, display it ?>
+        <div class="c-alert c-alert--error">
+            <span class="c-icon c-icon--small c-icon--error c-icon--white"></span>
+            <?php echo $response->get_error_message(); ?>
+        </div>
     <?php
     }
-  } else {
-    // the wp_remote_post() function returned an error, display it
-    ?>
+} elseif ( !empty( $_POST['club'] ) ) { // terms and conditions need to be accepted ?>
     <div class="c-alert c-alert--error">
-      <span class="c-icon c-icon--small c-icon--error c-icon--white"></span>
-      <?php echo $response->get_error_message(); ?>
+        <span class="c-icon c-icon--small c-icon--error c-icon--white"></span>
+        <?php esc_html_e( 'You must accept the terms &amp; conditions', 'ccw_countries' ); ?>
     </div>
-    <?php
-  }
-} elseif ( !empty( $_POST['club'] ) ) {
-  // terms and conditions need to be accepted
-  ?>
-  <div class="c-alert c-alert--error">
-    <span class="c-icon c-icon--small c-icon--error c-icon--white"></span>
-    <?php esc_html_e( 'You must accept the terms &amp; conditions', 'ccw_countries' ); ?>
-  </div>
 <?php
 }
 ?>
 
 <form class="c-form" id="register-club" action="#" method="POST">
-  <fieldset class="c-form__fieldset">
+    <fieldset class="c-form__fieldset">
     <label class="c-form__label u-text--left" for="club[venue_attributes][name]"><?php esc_html_e( 'Venue name', 'ccw_countries' ); ?>:</label>
     <input class="c-form__input" id="club[venue_attributes][name]" name="club[venue_attributes][name]" type="text" value="<?php echo isset( $_POST['club']['venue_attributes']['name'] ) ? htmlspecialchars( $_POST['club']['venue_attributes']['name'] ) : ''; ?>">
 
@@ -93,9 +86,9 @@ if ( empty( $_POST['honeypot'] ) && !empty( $_POST['terms-checkbox'] ) ) {
 
     <label class="c-form__label u-text--left" for="club[contact_attributes][skype]"><?php esc_html_e( 'Club Skype username', 'ccw_countries' ); ?>:</label>
     <input class="c-form__input" id="club[contact_attributes][skype]" name="club[contact_attributes][skype]" type="text" value="<?php echo isset( $_POST['club']['contact_attributes']['skype'] ) ? htmlspecialchars( $_POST['club']['contact_attributes']['skype'] ) : ''; ?>">
-  </fieldset>
-  <hr>
-  <fieldset class="c-form__fieldset">
+    </fieldset>
+    <hr>
+    <fieldset class="c-form__fieldset">
     <label class="c-form__label u-text--left" for="club[contact_attributes][name]"><?php esc_html_e( 'Your name', 'ccw_countries' ); ?>:</label>
     <input class="c-form__input" id="club[contact_attributes][name]" name="club[contact_attributes][name]" type="text"  value="<?php echo isset( $_POST['club']['contact_attributes']['name'] ) ? htmlspecialchars( $_POST['club']['contact_attributes']['name'] ) : ''; ?>">
 
@@ -118,11 +111,11 @@ if ( empty( $_POST['honeypot'] ) && !empty( $_POST['terms-checkbox'] ) ) {
     <!-- anti-spam field start-->
       <div style="display: none;">
         <label>Keep this field blank</label>
-        <input type="text" name="honeypot" id="honeypot" />
+        <input type="text" name="honeypot" id="honeypot">
       </div>
     <!-- anti-spam field end -->
-  </fieldset>
-  <fieldset class="c-form__fieldset">
+    </fieldset>
+    <fieldset class="c-form__fieldset">
     <button class="c-button c-button--green" type="submit" form="register-club" value="Create a Code Club"><?php esc_html_e( 'Create a Code Club', 'ccw_countries' ); ?></button>
-  </fieldset>
+    </fieldset>
 </form>
