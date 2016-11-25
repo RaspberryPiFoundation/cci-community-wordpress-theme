315e8115a8177ed0f4992017f0d0c12b02c65679
Yulia Bulgakova <yulia_bulgakova@hotmail.com>
2016-11-25 15:50:03 +0000

    Style guide UI

--- template-parts/page-matching.php
+++ template-parts/page-matching.php
@@ -30,54 +30,65 @@ if (empty( $_POST['body_text'] ) && !empty($_POST['address'])) {
 
 ?>
 
-
-<form class="c-col c-col--12" id="find-hosts" action="#" method="POST">
-
-  <!-- anti-spam field start -->
-  <div style="display: none;">
-    <!-- the field name is purposefully *not* something obvious, such as "honeypot" -->
-    <label for="body_text">Keep this field blank</label>
-    <input type="text" name="body_text" id="body_text" value="<?php echo isset($_POST['address']) ? $_POST['address'] : ''; ?>">
+<div class="c-page-block">
+  <h1 class="u-text--center">Find Club</h1>
+  <div class="c-grid c-grid--h-center">
+    <div class="c-col c-col--6">
+      <p>Let's find a Club near you.</p>
+      <form  id="find-hosts" action="#" method="POST">
+        <!-- anti-spam field start -->
+        <div style="display: none;">
+          <!-- the field name is purposefully *not* something obvious, such as "honeypot" -->
+          <label for="body_text">Keep this field blank</label>
+          <input type="text" name="body_text" id="body_text">
+        </div>
+        <!-- anti-spam field end -->
+        <label class="c-form__label" for="address">Address</label>
+        <input class="c-form__input u-margin--none" type="text" name="address" id="address"
+                value="<?php echo isset($_POST['address']) ? $_POST['address'] : ''; ?>"/>
+        <p class="u-text--center u-margin-top--base">
+          <button class="c-button c-button--green" type="submit" form="find-hosts">
+            Search
+          </button>
+        </p>
+      </form>
+    </div>
   </div>
-  <!-- anti-spam field end -->
-
-  <div class="c-col c-col--8">
-    <input class="c-form__input" type="text" name="address" id="address" placeholder="Enter your address"/>
-  </div>
-
-  <div class="c-col c-col--2">
-    <button class="c-button c-button--green" type="submit" form="find-hosts">
-      Find
-    </button>
-  </div>
-
-</form>
-
+</div>
 
 
 <?php if (isset($code_clubs)): ?>
-  <?php foreach ($code_clubs as $code_club): ?>
-    <div class="c-card c-col c-col--4">
-      <div class="c-card__body">
-        <h4><?php echo $code_club['name']; ?></h4>
-        <p class="c-meta">
-          <?php echo $code_club['address']['address_1'] . ' ' .
-            $code_club['address']['address_2'] . ' ' .
-            $code_club['address']['city'] . ' ' .
-            $code_club['address']['postcode']; ?>
-        </p>
+  <div class="c-page-block c-page-block--alt-block">
+    <h2 class="u-text--center">Search results: <?php echo sizeof($code_clubs);?></h2>
+    <div class="c-grid c-grid--h-center">
+      <?php foreach ($code_clubs as $code_club): ?>
+        <div class="c-card c-col c-col--4">
+          <div class="c-card__body">
+            <h4><?php echo $code_club['name']; ?></h4>
+            <p class="c-meta">
+              <?php echo $code_club['address']['address_1'] . ' ' .
+                $code_club['address']['address_2'] . ' ' .
+                $code_club['address']['city'] . ' ' .
+                $code_club['address']['postcode']; ?>
+            </p>
 
-        <?php if (!empty($code_club['phone'])): ?>
-          <span><?php echo $code_club['phone']; ?> </span>
-        <?php endif; ?>
-        <?php if (!empty($code_club['url'])): ?>
-          <p>
-            <a href="<?php echo $code_club['url']; ?>">Website</a>
-          </p>
-        <?php endif; ?>
-<!--        <a href="#" class="c-button c-button--hollow c-button--green">Contact Volunteer</a>-->
-      </div>
+            <?php if (!empty($code_club['phone'])): ?>
+              <span><?php echo $code_club['phone']; ?> </span>
+            <?php endif; ?>
+            <?php if (!empty($code_club['url'])): ?>
+              <p>
+                <a href="<?php echo $code_club['url']; ?>">Website</a>
+              </p>
+            <?php endif; ?>
+          </div>
+          <div class="c-card__footer">
+            <a class="c-card__link" href="#">
+              Contact Club Host
+            </a>
+          </div>
+        </div>
+      <?php endforeach; ?>
     </div>
-  <?php endforeach; ?>
+  </div>
 <?php endif; ?>
 
