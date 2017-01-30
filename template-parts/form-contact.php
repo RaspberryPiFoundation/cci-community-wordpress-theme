b0d779bbac57fc08e9df4b0ceadd1793dfe7cab8
Yulia Bulgakova <yulia_bulgakova@hotmail.com>
2017-01-30 13:42:51 +0000

    Tidy up contact form

--- template-parts/form-contact.php
+++ template-parts/form-contact.php
@@ -8,72 +8,69 @@
 
   if (!empty($_POST['message_body'])) {
 
-    $message = $_POST['message_body'] . __(' Please reply to ', 'ccw_countries') . $_POST['email'];
+    $message = $_POST['message_body'] . __(' Please reply to ', 'ccw_countries') . $_POST['name'] . '(' . $_POST['email'] . ')';
+
+    $email_title = __('Volunteer has contacted you.', 'ccw_countries');
 
     wp_mail($code_club['contact']['email'],
-            __('Volunteer has contacted you.', 'ccw_countries'),
+            $email_title,
             $message);
+
+    $flash_message = Flash_Message::Singleton();
+    $flash_message->createSuccess(__('Email has been sent', 'ccw_countries'));
   }
 
 ?>
 
-<div class="c-page-block">
-  <h1 class="u-text--center"><?php esc_html_e('Contact Club Host', 'ccw_countries'); ?></h1>
-  <div class="c-grid c-grid--h-center">
-    <div class="c-col c-col--8">
-      <div class="c-content-panel">
-        <h2 class="u-text--center">
-          <?php echo $code_club['venue']['name']; ?><br>
-          <span class="c-meta">
-             <?php echo $code_club['venue']['address']['address_1'] . ' ' .
-               $code_club['venue']['address']['address_2'] . ' ' .
-               $code_club['venue']['address']['city'] . ' ' .
-               $code_club['venue']['address']['postcode'];; ?>
-          </span>
-        </h2>
+<div class="c-content-panel">
+  <h2 class="u-text--center">
+    <?php echo $code_club['venue']['name']; ?><br>
+    <span class="c-meta">
+      <?php echo $code_club['venue']['address']['address_1'] . ' ' .
+                 $code_club['venue']['address']['address_2'] . ' ' .
+                 $code_club['venue']['address']['city'] . ' ' .
+                 $code_club['venue']['address']['postcode'];; ?>
+    </span>
+  </h2>
 
-        <p>
-          <?php  ?>
-        </p>
+  <form class="c-form" id="contact" action="#" method="POST">
 
-        <form class="c-form" id="contact" action="#" method="POST">
+    <?php echo $templates->render('input',
+      ['title' => __('Your name', 'ccw_countries'),
+        'attributes' => [
+          'id' => 'name',
+          'value' => use_if_set($_POST, ['name'], '', 'htmlspecialchars_with_quotes'),
+          'required' => ''
+        ]
+      ])
+    ?>
 
-          <?php echo $templates->render('input',
-            ['title' => __('Your name', 'ccw_countries'),
-              'attributes' => [
-                'id' => 'name',
-                'value' => use_if_set($_POST, ['name'], '', 'htmlspecialchars_with_quotes'),
-                'required' => ''
-              ]
-            ])
-          ?>
+    <?php echo $templates->render('input',
+      ['title' => __('Your email', 'ccw_countries'),
+        'attributes' => [
+          'id' => 'email',
+          'value' => use_if_set($_POST, ['email'], '', 'htmlspecialchars_with_quotes'),
+          'required' => ''
+        ]
+      ])
+    ?>
 
-          <?php echo $templates->render('input',
-            ['title' => __('Your email', 'ccw_countries'),
-              'attributes' => [
-                'id' => 'name',
-                'value' => use_if_set($_POST, ['name'], '', 'htmlspecialchars_with_quotes'),
-                'required' => ''
-              ]
-            ])
-          ?>
+    <label class="c-form__label" for="message_body">
+      <?php esc_html_e('Message', 'ccw_countries'); ?>
+      <span class="c-form__optional"><?php esc_html_e('(required)', 'ccw_countries'); ?></span>
+    </label>
+    <textarea class="c-form__textarea" cols="30" id="message_body" name="message_body" required="required" rows="10"></textarea>
 
-          <label class="c-form__label" for="message_body">
-            <?php esc_html_e('Message', 'ccw_countries'); ?>
-            <span class="c-form__optional"><?php esc_html_e('(required)', 'ccw_countries'); ?></span>
-          </label>
-          <textarea class="c-form__textarea" cols="30" id="message_body" name="message_body" required="required" rows="10"></textarea>
 
-          </p>
+   <?php echo $templates->render('submit-button',
+        [
+          'title' => __("Send Message", 'ccw_countries'),
+          'attributes' => [
+            'id' => 'contact-submit',
+            'form' => 'contact'
+          ]
+        ]) ?>
 
-          <p class="u-text--center">
-            <button class="c-button c-button--green" data-disable-with="Please wait..." type="submit">
-              <?php esc_html_e("Send Message", 'ccw_countries'); ?>
-            </button>
+  </form>
 
-          </p>
-        </form>
-
-      </div>
-  </div>
 </div>
