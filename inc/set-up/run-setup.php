c3af71efa64fbb7b45c48414c2c269412d262acb
Yulia Bulgakova <yulia_bulgakova@hotmail.com>
2017-02-24 13:35:16 +0000

    Flags to seperately enable log in & find venue pages set up

--- inc/set-up/run-setup.php
+++ inc/set-up/run-setup.php
@@ -1,10 +1,17 @@
 <?php
 
 require get_template_directory() . '/inc/set-up/required-pages-setup.php';
-
-add_action('init','create_all_pages');
-
 require get_template_directory() . '/inc/set-up/menu-setup.php';
 
-add_filter('wp_nav_menu_items', 'add_find_club_menu_link', 10, 2);
-add_filter('wp_nav_menu_items', 'add_sign_in_menu_link', 10, 2);
+
+if (GENERATE_LOG_IN_PAGES) {
+  echo GENERATE_LOG_IN_PAGES;
+  add_action('init','create_log_in_pages');
+  add_filter('wp_nav_menu_items', 'add_sign_in_menu_link', 10, 2);
+}
+
+if (GENERATE_FIND_VENUE_PAGES) {
+  add_action('init','create_find_venue_pages');
+  add_filter('wp_nav_menu_items', 'add_find_club_menu_link', 10, 2);
+}
+
