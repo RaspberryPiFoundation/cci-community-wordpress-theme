0adb6f275a12d4764d25af4e1390910309d3ad1b
Felipe Fernandes <felipefernandes@estudiotouch.com>
2017-10-27 15:23:00 -0200

    updating build scripting, now the theme, plugin and wp instalation are automagic

--- build.sh
+++ build.sh
@@ -1,4 +1,40 @@
 #!/bin/bash
-echo "Hora de começar a brincadeira..."
+echo "Code Club Brasil - wordpress"
+echo "================================"
+echo ""
+echo "Instanciamento e construção docker..."
+echo ""
 docker-compose build
-docker-compose up -d
+docker-compose up -d --remove-orphans
+sleep 20
+
+# Instalação do Wordpress
+echo "Instalação wordpress..."
+echo ""
+docker-compose exec wordpress wp core install --url="localhost:9000" --title="Code Club Brasil" --admin_user="ccbr" --admin_password="letscode" --admin_email="admin@codeclubbrasil.org.br"
+echo ""
+echo "Instalando plugin Advanced Custom Fields..."
+echo ""
+
+# Se não tiver instala o ACF
+docker-compose exec wordpress wp plugin install advanced-custom-fields
+docker-compose exec wordpress wp plugin activate advanced-custom-fields
+
+echo ""
+echo "Habilitando tema Code Club Brasil..."
+echo ""
+docker-compose exec wordpress wp theme activate ccbr-theme
+
+echo ""
+echo ""
+echo "+----------+-----------------------+"
+echo "+ Instalação INFOS   		         +"
+echo "+----------+-----------------------+"
+echo "+ hostname | http://localhost:9000 +"
+echo "+----------+-----------------------+"
+echo "+ username | ccbr 		         +"
+echo "+----------+-----------------------+"
+echo "+ password | letscode 	         +"
+echo "+----------+-----------------------+"
+echo ""
+echo "Bye!"
