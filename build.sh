246568de57bd1f0362c2304031154464cddf4c1d
Felipe Fernandes <felipefernandes@estudiotouch.com>
2017-10-27 16:04:22 -0200

    simplifying docker-compose and updating build.sh

--- build.sh
+++ build.sh
@@ -9,6 +9,8 @@ docker-compose up -d --remove-orphans
 sleep 20
 
 # Instalação do Wordpress
+echo ""
+echo ""
 echo "Instalação wordpress..."
 echo ""
 docker-compose exec wordpress wp core install --url="localhost:9000" --title="Code Club Brasil" --admin_user="ccbr" --admin_password="letscode" --admin_email="admin@codeclubbrasil.org.br"
@@ -17,7 +19,8 @@ echo "Instalando plugin Advanced Custom Fields..."
 echo ""
 
 # Se não tiver instala o ACF
-docker-compose exec wordpress wp plugin install advanced-custom-fields
+# docker-compose exec wordpress chown -R www-data:www-data /var/www
+# docker-compose exec wordpress wp plugin install advanced-custom-fields
 docker-compose exec wordpress wp plugin activate advanced-custom-fields
 
 echo ""
@@ -28,7 +31,7 @@ docker-compose exec wordpress wp theme activate ccbr-theme
 echo ""
 echo ""
 echo "+----------+-----------------------+"
-echo "+ Instalação INFOS   		         +"
+echo "+ Instalação INFOS   		     +"
 echo "+----------+-----------------------+"
 echo "+ hostname | http://localhost:9000 +"
 echo "+----------+-----------------------+"
