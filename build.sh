505ab5cb25232b7a2445a1a9dbfacf20b3c82bc3
Maurício Costa Pinheiro <mauricio.pinheiro@gmail.com>
2017-11-02 12:06:05 -0200

    fix pois não estava dando build no servidor

--- build.sh
+++ build.sh
@@ -13,21 +13,23 @@ echo ""
 echo ""
 echo "Instalação wordpress..."
 echo ""
-docker run -it --rm --volumes-from ccbrwordpresstheme_wordpress_1 --network container:ccbrwordpresstheme_wordpress_1 wordpress:cli \
+docker run -it --rm --volumes-from cc-wp --network container:cc-wp wordpress:cli \
 wp core install --url="localhost:9000" --title="Code Club Brasil" --admin_user="ccbr" --admin_password="letscode" \
 --admin_email="admin@codeclubbrasil.org.br" --skip-email
+
 echo ""
 echo "Instalando plugin Advanced Custom Fields..."
 echo ""
-
 # Se não tiver instala o ACF
-docker run -it --rm --volumes-from ccbrwordpresstheme_wordpress_1 --network container:ccbrwordpresstheme_wordpress_1 wordpress:cli \
+docker run -it --rm --volumes-from cc-wp --network container:cc-wp wordpress:cli \
 wp plugin activate advanced-custom-fields
+
 echo ""
 echo "Habilitando tema Code Club Brasil..."
 echo ""
-docker run -it --rm --volumes-from ccbrwordpresstheme_wordpress_1 --network container:ccbrwordpresstheme_wordpress_1 wordpress:cli \
+docker run -it --rm --volumes-from cc-wp --network container:cc-wp wordpress:cli \
 wp theme activate ccbr-theme
+
 echo ""
 echo ""
 echo "+----------+-----------------------+"
